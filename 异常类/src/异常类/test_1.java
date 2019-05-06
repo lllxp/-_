ac07840fc107838ff47baa49198888761f84a57f
lllxp <1261543653@qq.com>
2019-05-06 22:14:13 +0800

    first commit

--- 异常类/src/异常类/test_1.java
+++ 异常类/src/异常类/test_1.java
@@ -1,9 +1,22 @@
 package “Ï≥£¿‡;
+class MyException extends Exception{
+	
+	public MyException(String str) {
+		super(str);
+	}
+	
+	public String toString() {
+		return "Exception in thread"+Thread.currentThread().getName()+" "+"For input string:"+super.getMessage();
+	}
+}
+
 class MyTool{
-	public static int parseInt(String str){
+	
+	public static int parseInt(String str) throws MyException{
 		int result=0;
 		int i;
-		for(i=0;i<str.length();i++) {
+		
+		for(i=0;i<=str.length();i++) {
 			char ch=str.charAt(i);
 			if(ch>='0'&&ch<='9')
 				result=result*10+ch-'0';
@@ -18,8 +31,12 @@ public class test_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(MyTool.parseInt("\"111a\""));
+		try {
+			System.out.println(MyTool.parseInt("\"111a\""));
+		}
+		catch(MyException a){
+			a.printStackTrace();
+		}
 	}
 
 }
-
